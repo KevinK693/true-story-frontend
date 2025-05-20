66e5d8306a18278761fc6ef24c2ff7e7c7fb1f90
paulinepasseri <paulinecpasseri@gmail.com>
2025-05-20 09:51:26 +0200

    navigation base

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import HomeScreen from "./screens/HomeScreen";
+import ConnexionScreen from "./screens/ConnexionScreen";
+import GamesScreen from "./screens/GamesScreen";
 
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
@@ -12,16 +16,42 @@ const store = configureStore({
   reducer: {  },
 });
 
-// const Stack = createNativeStackNavigator();
-// const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator screenOptions={({ route }) => ({
+      tabBarIcon: ({ color, size }) => {
+        let iconName = '';
+
+        if (route.name === 'Map') {
+          iconName = 'location-arrow';
+        } else if (route.name === 'Places') {
+          iconName = 'map-pin';
+        }
+
+        return <FontAwesome name={iconName} size={size} color={color} />;
+      },
+      tabBarActiveTintColor: '#ec6e5b',
+      tabBarInactiveTintColor: '#335561',
+      headerShown: false,
+    })}>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Games" component={GamesScreen} />
+    </Tab.Navigator>
+  );
+};
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Connexion" component={ConnexionScreen} />
+          <Stack.Screen name="TabNavigator" component={TabNavigator} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
