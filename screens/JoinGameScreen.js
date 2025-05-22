7e4911e14f7e605dca516475b3b1374f25c1f70e
Your name <Your email>
2025-05-22 10:17:03 +0200

    profil in JoinScreen

--- screens/JoinGameScreen.js
+++ screens/JoinGameScreen.js
@@ -8,50 +8,89 @@ import {
   TouchableOpacity,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import { removeToken, updateAvatar } from "../reducers/user";
 
 export default function JoinGame({ navigation }) {
   const [code, setCode] = useState("");
 
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user.value);
+  const token = user.token;
+  const BACKEND_URL = process.env.BACKEND_URL;
+
+  useEffect(() => {
+    if (token) {
+      fetch(`${BACKEND_URL}/users/${token}`)
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.result && data.user.avatar) {
+            // Si l'avatar du serveur est différent de celui dans Redux, mettre à jour Redux
+            if (data.user.avatar !== user.avatar) {
+              dispatch(updateAvatar(data.user.avatar));
+            }
+          } else {
+            console.log("Erreur de récupération de l'image");
+          }
+        });
+    }
+  }, [token]);
+
+  const avatarUrl = user.avatar;
+
+  const handleLogout = () => {
+    dispatch(removeToken());
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-    
-      <Image style={styles.user} source={require("../assets/avatar.png")} />
-      
-      <Text style={styles.textJoin}>Rejoindre une partie</Text>
-      <Text style={styles.textEnter}>Entrez un code</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Code de la partie"
-        placeholderTextColor="#000"
-        onChangeText={(text) => setCode(text)}
-        value={code}
-      />
-      <TouchableOpacity
-        onPress={() => navigation.navigate()}
-        style={styles.button}
-        activeOpacity={0.8}
-      >
-        <Text style={styles.buttonText}>Rejoindre</Text>
-      </TouchableOpacity>
-    </SafeAreaView>  );
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => navigation.navigate("Profile")}>
+          <Image style={styles.user} source={{ uri: avatarUrl }} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleLogout()}>
+          <FontAwesome5 name="sign-out-alt" size={30} color="#335561" />
+        </TouchableOpacity>
+      </View>
+      <View style={{ alignItems: "center", marginTop: 50 }}>
+        <Text style={styles.textJoin}>Rejoindre une partie</Text>
+        <Text style={styles.textEnter}>Entrez un code</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Code de la partie"
+          placeholderTextColor="#000"
+          onChangeText={(text) => setCode(text)}
+          value={code}
+        />
+        <TouchableOpacity
+          onPress={() => navigation.navigate()}
+          style={styles.button}
+          activeOpacity={0.8}
+        >
+          <Text style={styles.buttonText}>Rejoindre</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     backgroundColor: "#FBF1F1",
     padding: 20,
   },
   user: {
-    width: 65,
-    height: 65,
+    width: 55,
+    height: 55,
     borderRadius: 50,
-    marginBottom: 20,
-    position: "absolute",
-    top: 70,
-    left:40,
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   textJoin: {
     fontSize: 30,
